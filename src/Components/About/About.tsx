6022a2da9625183f265ede09928f11b2e7e2c46f
suyash-5613 <suyashsahu0405@gmail.com>
2026-08-20 01:22:22 +0530

    fixed download

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -120,20 +120,18 @@ function About() {
       </div>
 
       <div className="resume-area">
-        <button className="resume-btn">
+        <a
+          href={`${process.env.PUBLIC_URL}/Suyash_Sahu_Resume.pdf`}
+          download="Suyash_Sahu_Resume.pdf"
+          className="resume-btn"
+        >
           <svg className="resume-icon" viewBox="0 0 512 512">
             <path d="M0,179.714v248.969c0,13.568,11.101,24.669,24.669,24.669h50.632V155.107H24.669C11.101,155.107,0,166.146,0,179.714z" />
             <path d="M487.393,155.107h-50.632v298.245h50.632c13.506,0,24.607-11.101,24.607-24.669V179.714C512,166.146,500.899,155.107,487.393,155.107z" />
             <path d="M357.765,81.621c0-12.668-10.305-22.973-22.97-22.973h-157.57c-12.672,0-22.976,10.305-22.976,22.973v73.486h-30.101v298.245h263.709V155.107h-30.09V81.621z M197.418,101.818h117.177v53.289H197.418V101.818z" />
           </svg>
-          <a
-            href={`${process.env.PUBLIC_URL}/Data_Analytics_Resume.pdf`}
-            download="Suyash-Sahu-Resume"
-            className="resume-text"
-          >
-            Download Resume
-          </a>
-        </button>
+          <span className="resume-text">Download Resume</span>
+        </a>
       </div>
     </section>
   );
